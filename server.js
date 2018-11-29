daaf96fc7b4f0dcf2faecd0f851c8178d6a5885e
Дмитрий@janguaruy <dg7301408@gmail.com>
2018-11-29 07:07:03 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+
+  res.render("home.hbs", {
+    title: "Home Page",
+    welcome: "Welcome to Node express server"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    title: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send('<h1 style="color:red;">404 PAGE NOT FOUNT</h1>');
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
